1d435a2186b98069923a57771c4e52c9b316f72f
Canan Ağbal <agbalcanan8069@gmail.com>
2023-01-12 16:51:52 +0300

    mausemove

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 //event//
-document.querySelector(".container").addEventListener("mousemove", canan);
+document.querySelector("body").addEventListener("mousemove", canan);
+
+
 
 function canan(e) {
-    document.querySelector(".container").style.backgroundColor = 
-    `rgb(${e.clientX % 255}, ${e.clientY % 255}, ${(e.clientX + e.clientY) % 255})`
+    document.querySelector(".container1").style.backgroundColor = 
+    `rgb(${e.screenX % 255}, ${e.screenY % 255}, ${(e.screenX + e.screenY) % 255})`
 
+    document.querySelector(".container2").style.backgroundColor = 
+    `rgb(${e.screenX % 200}, ${e.screenY % 200}, ${(e.screenX + e.screenY) % 200})`
+
+    document.querySelector(".container3").style.backgroundColor = 
+    `rgb(${e.screenX % 150}, ${e.screenY % 150}, ${(e.screenX + e.screenY) % 150})`
+
+    document.querySelector(".container4").style.backgroundColor = 
+    `rgb(${e.screenX % 100}, ${e.screenY % 100}, ${(e.screenX + e.screenY) % 100})`
+    
+    document.querySelector("body").style.backgroundColor = 
+    `rgb(${e.screenX % 50}, ${e.screenY % 50}, ${(e.screenX + e.screenY) % 50})`
 }
